24dece99cec08a3b893ad5de903d365d3b0e42e4
kasperken <kennethandrews@protonmail.com>
2019-11-12 13:15:50 -0500

    :equipping weapons now works

--- lib/character.rb
+++ lib/character.rb
@@ -2,43 +2,27 @@ class Character
   
   def initialize(name)
     @name = name
-    @health = 100 
-    @inventory = {} 
+    @max_health = 100
+    @current_health = @max_health 
+    @inventory = [] 
     @equipped_weapon = 'fists' 
     @race = 'Human' 
   end
-
-  def name
-    @name
-  end
   
-  def add_to_inventory(item, new_ammount)
-    if @inventory[item]
-      @inventory[item] += new_ammount 
-
-    else
-      @inventory[item] = new_ammount
-    end
-  end
+  attr_accessor :max_health, :current_health, :inventory, :equipped_weapon
  
-  def inventory
-   @inventory
-  end 
-  
-  def equip_weapon
-    @equipped_weapon = @inventory.key?
-  end
 
-  def weapon
-    @equipped_weapon
-  end
-
+ # I'm hoping to replace this working method with attr_accessor 
+ # def add_to_inventory(item, new_ammount)
+ #   if @inventory[item]
+ #     @inventory[item] += new_ammount 
+ #   else
+ #     @inventory[item] = new_ammount
+ #   end
+ # end
+ 
   def attack(enemy)
-    @equipped_weapon.damage(enemy, dice) 
-  end
-
-  def take_damage(ammount)
-    @health -= ammount
+    @equipped_weapon.attack(enemy) 
   end
 
 end
--- lib/weapon.rb
+++ lib/weapon.rb
@@ -1,8 +1,9 @@
 class Weapon
-
-  def damage(enemy)
-    
-    enemy.take_damage($dice.roll(@damage, @dice))
+  
+  def attack(enemy)
+    enemy.current_health -= ($dice.roll(@damage, @dice))
   end
+
+  attr_accessor :dice, :damage, :critical, :weapon_type, :weight
 
 end
