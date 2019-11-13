5564402000367be210960ff651bb6fb9c6ae6375
kasperken <kennethandrews@protonmail.com>
2019-11-13 13:18:40 -0500

    Added tracking mechanism for instances of dice

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,10 +1,18 @@
 class Dice
-   
+  
+  @@list = []
+
   def initialize(dice_sides, dice_ammount=1) 
     @sides = dice_sides 
     @ammount = dice_ammount
+    @name = "#{dice_ammount}d#{dice_sides}"
+    @@list << self
   end
-     
+   
+  def self.list #lists all instances of Dice class.
+    @@list
+  end
+
   def roll
     total = @ammount * @sides 
     rand(1..total)
